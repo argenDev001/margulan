5fa1eb9a819f2a9854e8add19714e9bf7001c7cd
argenDev001 <adiletuuluargen190605@gmail.com>
2022-11-05 06:35:16 +0600

    the and project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let swiper = new Swiper(".mySwiper", {
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev",
 
     },
 });
@@ -15,27 +14,9 @@ close.addEventListener("click", () => {
 })
 
 
-$(document).ready(function () {
-    //Menu button on click event
-    $('.mobile-nav-button').on('click', function () {
-        // Toggles a class on the menu button to transform the burger menu into a cross
-        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)").toggleClass("mobile-nav-button__line--1");
-        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)").toggleClass("mobile-nav-button__line--2");
-        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)").toggleClass("mobile-nav-button__line--3");
-
-        // Toggles a class that slides the menu into view on the screen
-        $('.mobile-menu').toggleClass('mobile-menu--open');
-        return false;
-    });
-});
 
 
-var burgerMenu = document.getElementById('burger-menu');
-var overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click',function(){
-    this.classList.toggle("close");
-    overlay.classList.toggle("overlay");
-});
+
 
 
 
